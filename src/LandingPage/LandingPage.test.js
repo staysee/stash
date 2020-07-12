ae0c5950761c6d6088ee1db091779f67b327e609
Stacey Sugiono <sugiono.stacey@gmail.com>
2020-07-12 14:27:56 -0700

    fixed failing smoke tests

--- src/LandingPage/LandingPage.test.js
+++ src/LandingPage/LandingPage.test.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LandingPage from './LandingPage';
+import { BrowserRouter } from 'react-router-dom';
 
 it ('renders without creashing', () => {
     // create DOM element to render the component into
     const div = document.createElement('div');
     // render the component, this is the actual test, if something is wrong it will show up here
-    ReactDOM.render(<LandingPage />, div);  
+    ReactDOM.render(
+        <BrowserRouter>
+            <LandingPage />
+        </BrowserRouter>,
+        div
+    );  
     // clean up code
     ReactDOM.unmountComponentAtNode(div);
 })
--- src/LandingPage/RegistrationForm/RegistrationForm.test.js
+++ src/LandingPage/RegistrationForm/RegistrationForm.test.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import RegistrationForm from './RegistrationForm';
 
 it ('renders without creashing', () => {
     // create DOM element to render the component into
     const div = document.createElement('div');
     // render the component, this is the actual test, if something is wrong it will show up here
-    ReactDOM.render(<RegistrationForm />, div);  
+    ReactDOM.render(<BrowserRouter><RegistrationForm /></BrowserRouter>, div);  
     // clean up code
     ReactDOM.unmountComponentAtNode(div);
 })
