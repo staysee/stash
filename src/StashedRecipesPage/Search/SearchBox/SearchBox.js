34a241db10a398c0b33d91e7b2d11ca90db046e8
Stacey Sugiono <sugiono.stacey@gmail.com>
2020-07-28 23:55:35 -0700

    search style

--- src/StashedRecipesPage/Search/SearchBox/SearchBox.js
+++ src/StashedRecipesPage/Search/SearchBox/SearchBox.js
@@ -2,17 +2,22 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
+import './SearchBox.css'
+
 class SearchBox extends React.Component {
     render() {
         return (
             <div className="SearchBox">
-                <FontAwesomeIcon icon={faSearch} />
-                <input 
-                    type="text" 
-                    placeholder="Search recipes..."
-                    value={this.props.searchTerm}
-                    onChange={e => this.props.updateSearchTerm(e.target.value)}
-                />
+                <div className="FormField">
+                    <FontAwesomeIcon icon={faSearch} />
+                    <input 
+                        type="text" 
+                        className="FormField__input"
+                        placeholder="Search recipes..."
+                        value={this.props.searchTerm}
+                        onChange={e => this.props.updateSearchTerm(e.target.value)}
+                    />
+                </div>
             </div>
         )
     }
--- src/StashedRecipesPage/Search/SearchFilter/SearchFilter.js
+++ src/StashedRecipesPage/Search/SearchFilter/SearchFilter.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import './SearchFilter.css'
+
 class SearchFilter extends React.Component {
     
     render() {
@@ -7,14 +9,18 @@ class SearchFilter extends React.Component {
 
         return (
             <div className="SearchFilter">
-                <select value={filterType}
-                    onChange={ e => this.props.updateFilterType(e.target.value)}
-                >
-                    <option value="All">All</option>
-                    <option value="Breakfast">Breakfast</option>
-                    <option value="Lunch">Lunch</option>
-                    <option value="Dinner">Dinner</option>
-                </select>
+                <div className="FormField">
+                    <select 
+                        className="FormField__select"
+                        value={filterType}
+                        onChange={ e => this.props.updateFilterType(e.target.value)}
+                    >
+                        <option value="All">All</option>
+                        <option value="Breakfast">Breakfast</option>
+                        <option value="Lunch">Lunch</option>
+                        <option value="Dinner">Dinner</option>
+                    </select>
+                </div>
             </div>
         )
     }
