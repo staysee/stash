29261eed2050f642b013a503473a78b2db4013f9
Stacey Sugiono <sugiono.stacey@gmail.com>
2020-11-06 13:01:18 -0800

    switched endpoint connection

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    // API_ENDPOINT: `https://stash-api-1.herokuapp.com/api`,
-    API_ENDPOINT: 'http://localhost:8000/api',
-    TOKEN_KEY: 'stash-auth-token'
+    API_ENDPOINT: 'https://stash-api-1.herokuapp.com/api',
+    // API_ENDPOINT: 'http://localhost:8000/api',
+    TOKEN_KEY: 'stash-auth-token',
 }
