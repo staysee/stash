1b08dd8910034e847b002a29519c3a2f78e20972
Stacey Sugiono <sugiono.stacey@gmail.com>
2020-11-16 21:56:18 -0800

    logo links to recipe home page

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/logo-stash.png';
 import './Logo.css';
 
 function Logo() {
   return (
     <div className="Logo">
-      <img src={logo} alt="Stash logo" />
+      <Link to="/recipes">
+        <img src={logo} alt="Stash logo" />
+      </Link>
     </div>
   );
 }
